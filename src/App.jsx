7c4d3109b48e720b48f2927d092a43adbcc2f2a4
Saoa35 <saoamah@gmail.com>
2023-04-22 08:07:30 +0200

    created Header comp. with slider

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.scss";
+import { Header } from "./Components/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Header />
+      </header>
+      <section></section>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
